13ac4dff021e5df31fb0a6c18e95dcbb29564699
banuArsh <technovice.007@gmail.com>
2023-09-24 12:33:24 +0530

    intial commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,34 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            What's destination next? Is it better technology? A better you? Or a
+            better community of makers?Wherever you desire to go,we give you the
+            tools,thechnology. So you are inspired to build what is next,ensure
+            your career never stand still, and navigate further together.Move
+            forward .Take the world with you.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
